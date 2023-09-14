395a626a1b0d37e39dcf5beef36db9a250cd327a
Leire Garmendia <lgarmendiadi20dw@ikzubirimanteo.com>
2023-09-14 09:50:15 +0200

    PHP Exercises - Block 1 finished

--- PHP Exercises - Block 1/10 Online store.php
+++ PHP Exercises - Block 1/10 Online store.php
@@ -1,8 +1,8 @@
 <?php
-$total_compra=30;
-// $tipo_compra="pets";
-$tipo_compra="clothes";
-
+$total_compra=30.5;
+$tipo_compra="pets";
+// $tipo_compra="clothes";
+$shipping=0;
 
 if($total_compra<19){
     if($tipo_compra=="pets"){
@@ -10,10 +10,24 @@ if($total_compra<19){
     }
     elseif( $tipo_compra=="clothes"){
         echo "Shipping costs are 9 euros";
+        $shipping=9;
     }
 }elseif($total_compra>=19 && $total_compra<80){
     echo "The shipping costs are 9 euros";
+    $shipping=9;
+    
+    if($tipo_compra=="pets"){
+        $total_compra=$total_compra+($total_compra*0.10)+$shipping;
+    }else{
+        $total_compra=$total_compra+($total_compra*0.21)+$shipping;
+    }
 }else{
     echo "The shipping costs are free";
+    if($tipo_compra=="pets"){
+        $total_compra=$total_compra+($total_compra*0.10);
+    }else{
+        $total_compra=$total_compra+($total_compra*0.21);
+    }
 }
+echo "Final priece ="$total_compra;
 ?>
