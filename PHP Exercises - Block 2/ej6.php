7769ee36e294aca3b6d790826c969342231dacc4
Leire Garmendia <lgarmendiadi20dw@ikzubirimanteo.com>
2023-09-20 14:27:13 +0200

    ej6 echo

--- PHP Exercises - Block 2/ej6.php
+++ PHP Exercises - Block 2/ej6.php
@@ -1,53 +1,30 @@
 <?php
-$cumpleaños=array("enero"=>array("Enero", "Ainara", "Xabi") , "febrero"=>array("febrero","Irati", "Ibai"), "marzo"=>array("marzo","Juan","María ","Alejandro"), "abril"=>array(    "Ana",
+$cumpleaños=array("enero"=>array( "Ainara", "Xabi") , "febrero"=>array("febrero","Irati", "Ibai"), "marzo"=>array("marzo","Juan","María ","Alejandro"), "abril"=>array(    "Ana",
 "abril","Carlos", "María"), "mayo"=>array(    "Marta" ,"Sergio" ,"Isabel" ) , "junio"=>array(    "Andrés" ,"Natalia" ,"Gabriel" ,"Susana" ,"Javier" ) , "julio"=>array(    "Carmen" ,"Rodrigo" ) , "agosto"=>array() , "septiembre"=>array() , "octubre"=>array() , "noviembre"=>array() , "diciembre"=>array());
 function agregarCumple($cumpleaños, $mes, $nombre) {
     $cumpleaños[$mes][] = $nombre;
-    
-    echo count($cumpleaños[$mes]);
+   return $cumpleaños;
+}
+function contPersonas($cumpleaños){
+    $contNombres=0;
+    foreach ($cumpleaños as $mes => $nombre) {
+        $contNombres+=count($nombre);
+        
+    }
+    echo "<p>En esta lista hay $contNombres nombres inscritos</p>";
 }
 
+$nombre="Leire";
+$mes="Diciembre";
+$cumpleaños=agregarCumple($cumpleaños, strtolower($mes), $nombre);
+contPersonas($cumpleaños);
 
-$nombre="paula";
-$mes="marzo";
-agregarCumple($cumpleaños, strtolower($mes), $nombre);
-
-
-
-
-
-
-
-echo "<table style='border:black solid 1px'>";
-echo "<tr>";
-         foreach(array_keys($cumpleaños) as $mes){
-
-            echo "<td style='border:black solid 1px; color: blue'>$mes</td>";
-            foreach($mes as $nombre){
-                echo "<tr>";
-                echo "<td style='border:black solid 1px'>$nombre</td>";
-                echo "</tr>";
-            }
+        foreach ($cumpleaños as $mes => $nombre) {
+          
+            echo "<p style=' color: blue'>$mes</p>";
+            echo "<p>" . implode("<br> ", $nombre) . "</p>";
+            // echo "<p>" . count($nombre) . "</p>";
+            ;
         }
-        
-// foreach($cumpleaños as $mes){
-         
-//         foreach($mes as $nombre){
-//             echo "<tr>";
-//             echo "<td style='border:black solid 1px'>$nombre</td>";
-//             echo "</tr>";
-//         }
-        
-//     }
-    echo "</tr>";
-    
   
-   
-  
-   
-   
- 
-    
-
-echo "</table>";
 ?>
