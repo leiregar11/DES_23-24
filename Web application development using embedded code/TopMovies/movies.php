ab6af29af1da754bc263f4775be3cd6c7c8af53b
Leire Garmendia <lgarmendiadi20dw@ikzubirimanteo.com>
2023-10-05 09:49:57 +0200

    top movies

--- Web application development using embedded code/TopMovies/movies.php
+++ Web application development using embedded code/TopMovies/movies.php
@@ -6,6 +6,7 @@
         private $punctuation;
         
         public function __construct($name,$isan,$year,$punctuation){
+            echo "public function __construct(name,isan,year,punctuation){";
             $this->name=$name;
             $this->isan=$isan;
             $this->year=$year;
@@ -13,48 +14,87 @@
         }
 
         public function getName(){
+            echo "public function getName(){";
             return $this->name;
         }
         public function getIsan(){
+            echo "public function getIsan(){";
             return $this->isan;
         }
         public function getYear(){
+            echo "public function getYear(){";
             return $this->year;
         }
         public function getPunctuation(){
+            echo "public function getPunctuation(){";
             return $this->punctuation;
         }
         public function __toString(){
+            echo "public function __toString(){";
             return "<td> ".($this->name)."</td> <td> ".($this->isan)."</td><td> ".($this->year)."</td><td> ".($this->punctuation)."</td>";
         }
     
     }
-    class Cinema{
+    class TopMovies{
         private $films=[];
         public function __construct($films=""){
             $this->films;
         }
-        public function addFilm($film){
-            $ISAN=$film->getIsan();
-            if(!array_key_exists($ISAN,$this->films) && strlen($ISAN)==8){
-                if($film->getName()!=""&&$film->getYear()!=""&&$film->getPunctuation()!="")
-            }else{
-                if(array_key_exists($ISAN,$this->films)){
-                    throw new Exception ("The ISAN already exists");
-                }else{
-                    throw new Exception ("The ISAN must have 8 digits.");
+        ////////////////////////////////////////////////////////////////
+
+        public function manager($film) {
+            echo "public function manager(film) {";
+            $isan = $film->getIsan();
+            if (strlen($isan) == 8) {
+                if (!array_key_exists($isan, $this->films)) {
+                    $this->addFilm($film, $isan);
+                } else {
+                    $this->updateOrDeleteFilm($film, $isan);
                 }
-                
+            } else {
+                throw new Exception("The ISAN must have 8 digits.");
             }
-            $this->
         }
-    }
-    if(!isset($_GET["name"]) && !isset($_GET["isan"])){
-        echo "Missing data, make sure that at least the name or the ISAN are entered.";
-        $personManager = new Cinema($_GET["hidden"]);
+        ////////////////////////////////////////////////////////////////
+        public function addFilm($film,$ISAN){
+            echo "public function addFilm(film,ISAN){";
+            if($film->getName()!="" || $film->getYear()!="" || $film->getPunctuation()!=""){
+                $this->films[$ISAN][]=$film;
+            }else{
+                throw new Exception ("Fill out all the fields");
+            }
+        }
+        public function updateOrDeleteFilm($film,$ISAN){
+            if($film->getName()!="" || $film->getYear()!="" || $film->getPunctuation()!=""){
+                $this->films[$ISAN][]=$film;
+            }else{
+                unset($this->films[$ISAN]);
+            }
+        }
+        public function printFilms($name){
+            foreach ($films as $film ) {
+                if (strcasecmp($name, $film->getName()) === 0) {
+                    echo $film->__toString();
+                } else {
+                    echo "La palabra '$palabraBuscada' no está en el string.";
+                }
+            }
+        }
 
-    }else{
-        
-    }
+        public function getFilms(){
+            return serialize($this->films);
+        }
+}
 
+
+
+if (!isset($_GET["name"]) && !isset($_GET["isan"])) {
+    echo "Missing data, make sure that at least the name or the ISAN are entered.";
+} else {
+    // Crear una nueva película con los datos del formulario y controlarla
+    $topMovies = new TopMovies($_GET["hidden"]);
+    $newMovie = new Movie($_GET["name"], $_GET["isan"], $_GET["year"], $_GET["punctuation"]);
+    $topMovies->manager($newMovie);
+}
+    
 ?>
--- Web application development using embedded code/TopMovies/view.php
+++ Web application development using embedded code/TopMovies/view.php
@@ -6,52 +6,55 @@
     <title>Top Movies - Leire</title>
 </head>
 <body>
-   
-    <div class="showInfo">
-        <table>
-            <tr>
-                <th>Name</th>
-                <th>ISAN</th>
-                <th>Year</th>
-                <th>Punctuation</th>
-            </tr>
-            <?php
-            include "movies.php";
-    
-            foreach ($cinema->getMovies() as $movie) {
-                
-                if ($cont % 2 == 0) {
-                    echo "<tr style='background-color: lightblue;'>";
+
+
+<div class="showInfo">
+    <table border="1">
+        <tr>
+            <th>Name</th>
+            <th>ISAN</th>
+            <th>Year</th>
+            <th>Punctuation</th>
+        </tr>
+        <?php
+        include "movies.php";
+        if(isset($topMovies)) {
+            $cont = 0;
+            foreach ($topMovies->getMovies() as $movies) {
+                foreach ($movies as $movie) {
+                    $cont++;
+                    if ($cont % 2 == 0) {
+                        echo "<tr style='background-color: lightblue;'>";
+                    } else {
+                        echo "<tr>";
+                    }
                     echo $movie->__toString();
-                
-                
-                echo "</tr>";
+                    echo "</tr>";
                 }
             }
-            ?>
-        </table>
-    </div>
+        }
+        ?>
+    </table>
+</div>
     <div class="enterInfo">
-        <form method="get"  acction="movies.php">
-        <label>Name: <input type="textbox" name="name"></label><br>
-        <label>ISAN: <input type="textbox" name="isan"></label><br>
-        <label>Year: <input type="textbox" name="year"></label><br>
+    <form method="get" action="movies.php">
+        <label>Name: <input type="text" name="name"></label><br>
+        <label>ISAN: <input type="text" name="isan"></label><br>
+        <label>Year: <input type="text" name="year"></label><br>
         <label>Punctuation: 
             <select name="punctuation">
-                <option value = "0" >0</option>
-                <option value = "1" >1</option>
-                <option value = "2" >2</option>
-                <option value = "3" >3</option>
-                <option value = "4" >4</option>
-                <option value = "5" >5</option>
+                <option value="0">0</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
             </select>
         </label><br>
         <input type="submit" name="send" value="send">
-        <input type="hidden" name="hidden" 
-            value="<?php echo htmlspecialchars($cinema->__toString()); ?>">
-        
+        <input type="hidden" name="hidden" value="<?php echo htmlspecialchars(serialize($topMovies)); ?>">
     </form>
-    </div>
+</div>
     
 </body>
 </html>
