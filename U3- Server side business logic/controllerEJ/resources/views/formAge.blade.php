bc9e633949083da67dd487ce32d239dfb58c0b1d
Leire Garmendia <lgarmendiadi20dw@ikzubirimanteo.com>
2023-10-24 09:50:11 +0200

    ejemplo views

---
+++ U3- Server side business logic/controllerEJ/resources/views/formAge.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="{{ route('form') }}" method="get">
+    <button type="submit">FORM</button>
+</form>
+<form action="{{ route('age') }}" method="get">
+    <button type="submit">AGE</button>
+</form>
+
+
+</body>
+</html>
